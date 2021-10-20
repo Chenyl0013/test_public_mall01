4185556b99fcf9a9d485b7e1f9856067ccc78c8c
06627 <m13760306627@gmail.com>
2021-10-20 15:00:21 +0800

    print phpinfo

--- index.php
+++ index.php
@@ -5,3 +5,9 @@ $basedata['username']='root';//--chenyl
 //$basedata['password']='root';//--alientaker
 $basedata['password']='123456';//--chenyl
 $basedata['port']=3306;//--alientaker
+
+//以下为06627修改
+
+print_r(phpinfo());
+
+
